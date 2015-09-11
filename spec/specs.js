f011a40892845dfe49f1ae6d31f8c6d7c9051f35
Jordan Navratil <jord.navratil@gmail.com>
2015-09-11 12:51:36 -0700

    fix equal to eql

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,13 @@
 describe('replaceWord', function(){
   it("replaces 'life' with 'dog' in the string input 'life'", function(){
-      expect(replaceWord('life','life', 'dog')).to.equal('dog');
+      expect(replaceWord('life','life', 'dog')).to.eql('dog');
   });
 
   it("replaces 'life' with 'dog' in the string input 'a life'", function(){
-    expect(replaceWord('a life','life', 'dog')).to.equal('a dog');
+    expect(replaceWord('a life','life', 'dog')).to.eql('a dog');
   });
 
   it("replaces 'life' with 'dog' in the string input 'get a life'", function(){
-    expect(replaceWord('get a life','life', 'dog')).to.equal('get a dog');
+    expect(replaceWord('get a life','life', 'dog')).to.eql('get a dog');
   });
 });
