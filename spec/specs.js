c888d748d333149addc67ab15471733750b7994e
Jordan Navratil <jord.navratil@gmail.com>
2015-09-11 12:14:45 -0700

    pass specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,13 @@
 describe('findAndReplace', function(){
+  it("replaces 'life' with 'dog' in the string input 'life'", function(){
+      expect(findAndReplace('life','life', 'dog')).to.equal('dog');
+  });
+
+  it("replaces 'life' with 'dog' in the string input 'a life'", function(){
+    expect(findAndReplace('a life','life', 'dog')).to.equal('a dog');
+  });
+
   it("replaces 'life' with 'dog' in the string input 'get a life'", function(){
     expect(findAndReplace('get a life','life', 'dog')).to.equal('get a dog');
   });
-  // 
-  // it("replaces 'life' with 'dog' in the string input 'a life'", function(){
-  //   expect(findAndReplace('cat tiger','cat', 'dog')).to.equal('dog tiger');
-  // });
-  //
-  // it("replaces 'cat' with 'dog' in the string input 'cat'", function(){
-  //   expect(findAndReplace('cat','cat', 'dog')).to.equal('dog');
-  // });
 });
